e4618e2854e3a96205b61ebed72cf7ad0e6097bc
seeyeon <gulf72pr@swu.ac.kr>
2024-12-03 23:16:54 +0900

    create SimpleDbTest , r

---
+++ src/test/java/com/ll/SimpleDbTest.java
@@ -0,0 +1,18 @@
+package com.ll;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.TestMethodOrder;
+
+@TestMethodOrder(MethodOrderer.MethodName.class)
+public class SimpleDbTest {
+
+    @BeforeAll
+    public static void beforeAll() {
+        simpleDb = new SimpleDb("localhost", "root", "lldj123414", "simpleDb__test");
+        simpleDb.setDevMode(true);
+
+        createArticleTable();
+    }
+
+}
