e1133eeab62a39ca8847e9d99f2d717320a0f081
seeyeon <gulf72pr@swu.ac.kr>
2024-12-03 23:24:24 +0900

    create SimpleDbTest , ãb

--- src/main/java/com/ll/SimpleDb/SimpleDb.java
+++ src/main/java/com/ll/SimpleDb/SimpleDb.java
@@ -1,19 +1,13 @@
 package com.ll.SimpleDb;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor  //final 키워드가 붙어있는 필드의 생성자 생성
 public class SimpleDb {
 
     private final String host;
     private final String username;
     private final String pwd;
     private final String dbName;
-
-    public SimpleDb(String host, String username, String pwd, String dbName) {
-        this.host = host;
-        this.username = username;
-        this.pwd = pwd;
-        this.dbName = dbName;
-    }
-
-
 
 }
